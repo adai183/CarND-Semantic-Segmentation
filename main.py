309f96426b7386e99b19d2b6a031c57fe58238fe
adai183 <andreas.daiminger@gmail.com>
2018-03-12 17:17:01 +0100

    debug

--- main.py
+++ main.py
@@ -70,8 +70,15 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
                                     kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     x = tf.layers.batch_normalization(x, training=True, name='batch_2')
 
+    # 1x1 convolution of vgg_layer4_out and batch normalization
+    vgg_layer4_out = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding= 'same',
+                                   name='conv_1x1_2',
+                                   kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                   kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    vgg_layer4_out = tf.layers.batch_normalization(vgg_layer4_out, training=True, name='batch_3')
+
     # skip connection
-    x = tf.add(x, vgg_layer4_out)
+    x = tf.add(x, vgg_layer4_out, name="skip_1")
 
     # upsampling and batch normalization
     x = tf.layers.conv2d_transpose(x, num_classes, 4, strides=(2, 2), padding="same",
@@ -79,6 +86,13 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
                                     kernel_initializer=tf.truncated_normal_initializer(stddev=0.1),
                                     kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     x = tf.layers.batch_normalization(x, training=True, name='batch_4')
+
+    # 1x1 convolution of vgg_layer3_out and batch normalization
+    vgg_layer3_out = tf.layers.conv2d(vgg_layer3_out, num_classes, 1,
+                                   padding= 'same',
+                                   kernel_initializer= tf.random_normal_initializer(stddev=0.01),
+                                   kernel_regularizer= tf.contrib.layers.l2_regularizer(1e-3))
+    vgg_layer3_out = tf.layers.batch_normalization(vgg_layer3_out, training=True, name='batch_2')
 
     # skip connection
     x = tf.add(x, vgg_layer3_out)
